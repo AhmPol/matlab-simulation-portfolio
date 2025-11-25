cb2e0fa64778aa267d9eb5f836e4805f7502910a
AhmPol <ahmselpol@gmail.com>
2025-11-25 18:26:01 -0500

    Plot sine and cosine waves with labels and grid
    Added plots for sine and cosine functions with different styles.

--- basics/sincos/sincos.m
+++ basics/sincos/sincos.m
@@ -1,1 +1,21 @@
+x = 0:0.01:2*pi;
+
+y1 = sin(2*x);
+y2 = sin(x);
+y3 = cos(2*x);
+y4 = cos(x);
+
+plot(x, y1, 'r', 'LineWidth', 2);  % red solid
+hold on
+plot(x, y2, 'b--', 'LineWidth', 2);  % blue dashed
+plot(x, y3, 'y', 'LineWidth', 2);   % yellow solid
+plot(x, y4, 'g--', 'LineWidth', 2); % green dashed
+hold off
+
+
+xlabel('x(radians)');
+ylabel ('y')
+title('Sine and Cosine Waves');
+
+grid on
 
